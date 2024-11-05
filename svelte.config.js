b50f434648b45c8cf6d67c6078de1ec343c8f62b
Jacob Hart <jacob@uc12825.ufralc.mb-per.uhb.fr>
2024-11-05 14:24:55 +0100

    deploy test

--- svelte.config.js
+++ svelte.config.js
@@ -6,12 +6,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-		}),
+		adapter: adapter(),
+		paths : {
+			base: process.env.NODE_ENV === 'production' ? '' : '',
+		}
 	}
 };
 
